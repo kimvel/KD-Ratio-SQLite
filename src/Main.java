b08e1f1b333dc9ac917c683644c76a71e8f2626d
kAN <kimern2@hotmail.com>
2019-07-15 23:40:58 +0200

    Commited

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.xml.ws.Response;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,34 +9,47 @@ import java.net.URL;
 
 public class Main {
 
-    String KEY = "";
+    public static final String SEARCH_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=2e4a25e9";
 
+    public static String sendGetRequest(String requestUrl){
+        StringBuilder response = new StringBuilder();
 
-        public static String sendGetRequest(String requestUrl){
-            StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "*/*");
+            connection.setRequestProperty("Content type", "application/json; charset=UTF-8");
 
-            try {
-                URL url = new URL(requestUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Accept", "*/*");
-                connection.setRequestProperty("Content type", "application/json; charset=UTF-8");
+            InputStream stream = connection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream);
+            BufferedReader buffer = new BufferedReader(reader);
 
-                InputStream stream = connection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(stream);
-                BufferedReader buffer = new BufferedReader(reader);
-
-            } catch(MalformedURLException e){
-                e.printStackTrace();
-            } catch(IOException e) {
-                e.printStackTrace();
+            String line;
+            while((line = buffer.readLine()) != null){
+                response.append(line);
             }
-            return response.toString();
+            buffer.close();
+            connection.disconnect();
 
+        } catch(MalformedURLException e){
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
         }
+        return response.toString();
+
+    }
+
+    public static String searchByTitle(String title, String key){
+        String requestUrl = SEARCH_URL .replaceAll("TITLE", title) .replaceAll("APIKEY", key);
+
+        return sendGetRequest(requestUrl);
+    }
 
 
     public static void main(String[] args) {
-        System.out.println("Hello Worlds!");
+        String jsonResponse = Main .searchByTitle("batman", "2e4a25e9");
+        System.out.println(jsonResponse);
     }
 }
